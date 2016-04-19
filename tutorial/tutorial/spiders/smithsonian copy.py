c590c04e3a018e17ba01f3ed5a11f2c06f802531
Errol E <errolelbasan@errols-mbp.home>
2016-04-19 03:07:51 -0400

    Minor Updates

--- tutorial/tutorial/spiders/smithsonian copy.py
+++ tutorial/tutorial/spiders/smithsonian copy.py
@@ -3,7 +3,7 @@ import scrapy
 from tutorial.items import DmozItem
 
 class DmozSpider(scrapy.Spider):
-    name = "dmoz1"
+    name = "smithsonian"
     allowed_domains = ["library.si.edu"]
     start_urls = [
         "http://library.si.edu/donate/major-supporters"
@@ -12,4 +12,5 @@ class DmozSpider(scrapy.Spider):
     def parse(self, response):
         for sel in response.xpath('//td/div'):
             title = sel.xpath('text()').extract()
+            
             print title
